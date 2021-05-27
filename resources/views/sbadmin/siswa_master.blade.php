68b013ca5b40116273a7963e47fbc5940f51690e
dwuldr <dwiwuldr017@gmail.com>
2021-05-27 15:19:39 +0700

    nilai

--- resources/views/sbadmin/siswa_master.blade.php
+++ resources/views/sbadmin/siswa_master.blade.php
@@ -62,7 +62,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="absen">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Absen</span></a>
+                    <span>Jadwal</span></a>
             </li>
 
             <li class="nav-item">
